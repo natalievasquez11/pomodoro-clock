6809b141dcd7ea9b3bab8db2a87055c490f2511f
Natalie Vasquez <natalievasquez11@gmail.com>
2022-02-24 15:23:33 -0600

    initial timer working

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import SessionLength from './components/sessionLengthComponent/SessionLength';
 import Session from './components/sessionComponent/Session';
 
 function App() {
+  const minsSecs = { minutes: 25, seconds: 0 };
+
   return (
     <div className="App">
       <BreakLength />
       <SessionLength />
-      <Session />
+      <Session minsSecs={minsSecs}/>
     </div>
   );
 }
--- src/components/sessionComponent/Session.js
+++ src/components/sessionComponent/Session.js
@@ -1,10 +1,34 @@
 import './session.css';
+import React, { useState, useEffect } from 'react';
 
-function Session() {
+function Session({ minsSecs }) {
+
+  const { minutes = 0, seconds = 0 } = minsSecs;
+  const [[mins, secs], setTime] = useState([minutes, seconds]);
+
+  const tick = () => {
+    if(mins === 0 && secs === 0) {
+      reset();
+    } else if(secs === 0) {
+      setTime([mins - 1, 59]);
+    } else {
+      setTime([mins, secs - 1]);
+    }
+  }
+
+  const reset = () => setTime([parseInt(minutes), parseInt(seconds)]);
+
+  useEffect(() => {
+    const timerId = setInterval(() => tick(), 1000);
+    return () => clearInterval(timerId);
+  })
+
+
+
   return (
     <div className="session-wrapper">
       <p id="timer-label">Session</p>
-      <p id="time-left">25:00</p>
+      <p id="time-left">{`${mins.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`}</p>
       <button id="start_stop">play pause</button>
       <button id="reset">reset</button>
     </div>
