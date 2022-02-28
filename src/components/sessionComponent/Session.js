c7899bcc7c053debaacd23f452eb0f1ef390d6ff
Natalie Vasquez <natalievasquez11@gmail.com>
2022-02-28 15:12:14 -0600

    play pause button working to pause and play timer

--- src/components/sessionComponent/Session.js
+++ src/components/sessionComponent/Session.js
@@ -3,14 +3,18 @@ import React, { useState, useEffect } from 'react';
 
 function Session({ minsSecs }) {
 
-  const { minutes = 0, seconds = 0 } = minsSecs;
+  const { minutes = 0, seconds = 0 } = minsSecs;  
   const [[mins, secs], setTime] = useState([minutes, seconds]);
+  const [isPaused, setPlayPause] = useState(true);
 
   const tick = () => {
+    //timer ended
     if(mins === 0 && secs === 0) {
       reset();
+    //seconds at zero, go to next minute
     } else if(secs === 0) {
       setTime([mins - 1, 59]);
+    //reduce time by one second
     } else {
       setTime([mins, secs - 1]);
     }
@@ -19,17 +23,19 @@ function Session({ minsSecs }) {
   const reset = () => setTime([parseInt(minutes), parseInt(seconds)]);
 
   useEffect(() => {
-    const timerId = setInterval(() => tick(), 1000);
+    const timerId = setInterval(() => {
+      if(!isPaused) {
+        tick();
+      }
+    }, 1000);
     return () => clearInterval(timerId);
-  })
-
-
+  });
 
   return (
     <div className="session-wrapper">
       <p id="timer-label">Session</p>
       <p id="time-left">{`${mins.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`}</p>
-      <button id="start_stop">play pause</button>
+      <button id="start_stop" onClick={() => setPlayPause(!isPaused)}>play pause</button>
       <button id="reset">reset</button>
     </div>
   )
