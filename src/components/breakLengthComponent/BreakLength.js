9d47ec7df2b01a1b3ad631d76706c6eb2606c618
Natalie Vasquez <natalievasquez11@gmail.com>
2022-02-28 21:40:44 -0600

    updated button icons

--- src/components/breakLengthComponent/BreakLength.js
+++ src/components/breakLengthComponent/BreakLength.js
@@ -4,9 +4,13 @@ function BreakLength({ breakLength, setBreakLength }) {
   return (
     <div className="length-wrapper">
       <div id="break-label">Break Length</div>
-      <button id="break-decrement" onClick={() => setBreakLength(breakLength - 1)}>break decrement</button>
+      <button id="break-decrement" onClick={() => setBreakLength(breakLength - 1)}>
+      <span className="material-icons">remove</span>
+      </button>
       <p id="break-length" className="length">{breakLength}</p>
-      <button id="break-increment" onClick={() => setBreakLength(breakLength + 1)}>break increment</button>
+      <button id="break-increment" onClick={() => setBreakLength(breakLength + 1)}>
+      <span className="material-icons">add</span>
+      </button>
     </div>
   )
 }
--- src/components/sessionLengthComponent/SessionLength.js
+++ src/components/sessionLengthComponent/SessionLength.js
@@ -4,9 +4,13 @@ function SessionLength({ sessionLength, setSessionLength }) {
   return(
     <div className="length-wrapper">
       <div id="session-label">Session Length</div>
-      <button id="session-decrement" onClick={() => setSessionLength(sessionLength - 1)}>session decrement</button>
+      <button id="session-decrement" onClick={() => setSessionLength(sessionLength - 1)}>
+      <span className="material-icons">remove</span>
+      </button>
       <p id="session-length" className="length">{sessionLength}</p>
-      <button id="session-increment" onClick={() => setSessionLength(sessionLength + 1)}>session increment</button>
+      <button id="session-increment" onClick={() => setSessionLength(sessionLength + 1)}>
+        <span className="material-icons">add</span>
+      </button>
     </div>
   )
 }
