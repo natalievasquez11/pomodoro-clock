37a135edf2b3440998e84565201ac9581730f85d
Natalie Vasquez <natalievasquez11@gmail.com>
2022-03-03 17:29:44 -0600

    updated breaklength increment and decrement button onClicks

--- src/components/breakLengthComponent/BreakLength.js
+++ src/components/breakLengthComponent/BreakLength.js
@@ -2,30 +2,22 @@ import './breakLength.css';
 
 function BreakLength({ breakLength, setBreakLength, isPaused }) {
 
-  const handleSetBreakLength = (e) => {
-    if(e.target.className.includes("decrement")) {
-      setBreakLength(breakLength - 1);
-    } else if(e.target.className.includes("increment")) {
-      setBreakLength(breakLength + 1);
-    }
-  }
-
   return (
     <div className="length-wrapper">
       <div id="break-label">Break Length</div>
       <div className="inc-dec-wrapper">
-        <button id="break-decrement" className="decrement"
-        onClick={(e) => handleSetBreakLength(e)}
+        <button id="break-decrement"
+        onClick={() => setBreakLength(breakLength - 1)}
         disabled={!isPaused || breakLength === 1}>
-          <span className="material-icons decrement">remove</span>
+          <span className="material-icons">remove</span>
         </button>
         <span className="length-span">
           <p id="break-length" className="length">{breakLength}</p>
         </span>
-        <button id="break-increment" className="increment"
-        onClick={(e) => handleSetBreakLength(e)}
+        <button id="break-increment"
+        onClick={() => setBreakLength(breakLength + 1)}
         disabled={!isPaused || breakLength === 60}>
-          <span className="material-icons increment">add</span>
+          <span className="material-icons">add</span>
         </button>
       </div>
     </div>
